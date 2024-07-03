8ae626c8a48d42195d15b3abb009424e9bbf295c
Xegram <78540186+xegram-0@users.noreply.github.com>
2024-07-03 21:28:06 +0700

    Create bagels.py

---
+++ bagels.py
@@ -0,0 +1,6 @@
+# Guess a secret three-digit number based on clues
+# Responses to your guess will be the following:
+# Pico for correct digit at wrong place
+# Fermi when guessing the right place and right digit
+# Bagels means no right digits
+# You have 10 tries
