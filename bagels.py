4e48cd04815c9d448a190eb60141ac16cba66e98
Hong Nhan <nguyenhongnhan9a5@gmail.com>
2024-07-05 17:08:56 +0700

    Basic funtional project

--- bagels.py
+++ bagels.py
@@ -7,13 +7,19 @@
 
 
 import random
-import secrets
 
 MAX_DIGITS = 3
 MAX_ATTEMPTS = 10
 
+def get_clue(yourGuess,the_number):
+    for i in range(MAX_DIGITS):
+        if yourGuess[i] == the_number[i]:
+            print("Fermi")
+        elif yourGuess[i] in the_number:
+            print("Pico")
+        else:
+            print("Bagels")
 def main():
-    guess:bool = True
     print("***************************")
     print("Welcome to bagels game!")
     print("Guess a 3-digit number with the following clues:")
@@ -22,18 +28,20 @@ def main():
     print("You get bagels when the digit is incorrect")
     print("Let's start the game!")
     print("***************************")
-    while True:
-        secret_gen = secrets.SystemRandom()
-        the_number = secret_gen.randint(100,999)
-        yourAttempts = 0
-        while yourAttempts < MAX_ATTEMPTS:
-            yourGuess = get_number()
-            evaluate_yourGuess(yourGuess)
-            
 
-       
-        
-        
-
+    the_number = str(random.randint(100,999))
+    yourAttempts = 1
+    while yourAttempts <= MAX_ATTEMPTS:
+        yourGuess = input("> ")
+        if yourGuess == the_number:
+            print("Bingo!!!")
+            break
+        else:
+            get_clue(yourGuess,the_number)
+        yourAttempts += 1
+        if yourAttempts > MAX_ATTEMPTS:
+            print("You have exceeded your attempt")
+            print("You lose!")   
+    print("Do you want to continue? (y/n)")      
 if "__main__" == __name__:
     main()
