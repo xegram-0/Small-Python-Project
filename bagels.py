a586b807101345c4e301b2977f4f818a572959b0
Hong Nhan <nguyenhongnhan9a5@gmail.com>
2024-07-05 17:47:30 +0700

    Fixing exceeding digits, added loop to the game, added infomative prompts, fixing the logical functions

--- bagels.py
+++ bagels.py
@@ -9,11 +9,15 @@
 import random
 
 MAX_DIGITS = 3
-MAX_ATTEMPTS = 10
+MAX_ATTEMPTS = 3
 
 def get_clue(yourGuess,the_number):
+    # If all correct
+    if yourGuess == the_number:
+        return "Bingo"
     # Check the digit in the number is equal to the secret number
     # elif ... in would shows if there is a right digit at wrong place
+    # This is the ease mode of the game
     for i in range(MAX_DIGITS):
         if yourGuess[i] == the_number[i]:
             print("Fermi")
@@ -33,24 +37,45 @@ def main():
     print("Let's start the game!")
     print("***************************")
 
-    # Using slicing requires both varibles to be string
-    the_number = str(random.randint(100,999))
-    yourAttempts = 1
+   
+    # This is where the game is running
+    while True:
+         # Using slicing requires both varibles to be string
+        print("New number has been generated!")
+        the_number = str(random.randint(100,999))
+        yourAttempts = 1
+        # This is the part where the guessing happens
+        while yourAttempts <= MAX_ATTEMPTS:
+            print(f"You have {MAX_ATTEMPTS} attempts.")
+            yourGuess=""
+            # Right format of number
+            # Else loop for eternity
+            while len(yourGuess) != MAX_DIGITS or not yourGuess.isdecimal():
+                print(f"Your guess #{yourAttempts}:")
+                yourGuess = input("> ")
 
-    while yourAttempts <= MAX_ATTEMPTS:
-        yourGuess = input("> ")
-        if yourGuess == the_number:
-            print("Bingo!!!")
-            break
-        else:
             get_clue(yourGuess,the_number)
-    # Need to be before if otherwise exceed the attempt limit
-        yourAttempts += 1
-        if yourAttempts > MAX_ATTEMPTS:
-            print("You have exceeded your attempt")
-            print("You lose!")
 
+        # Need to be before if otherwise exceed the attempt limit
+            yourAttempts += 1
+            if yourAttempts > MAX_ATTEMPTS:
+                print("You have exceeded your attempts")
+                print(f"The number is {the_number}")
+                print("You lose!")
+        
+        print("Do you want to play again? (y/n)")
+
+        
+        # Work with the 'n' but not 'y'
+        # Later on, this also worked???
+        choice = input("> ")
+        if choice.lower() != 'y':
+            break
+
+        # This time the code worked???
+
+        # if not input('> ').lower().startswith('y'):
+        #     break
     print("Thank you for playing!")
-    
 if "__main__" == __name__:
     main()
