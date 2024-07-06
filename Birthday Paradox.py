08a59e3791140aa07057a2085b8514fda237fb11
Hong Nhan <nguyenhongnhan9a5@gmail.com>
2024-07-06 20:13:00 +0700

    Created project

---
+++ Birthday Paradox.py
@@ -0,0 +1,6 @@
+"""
+Birthday Problem is about people having the same birthday.
+In a group of 70 people, there is 99.9% of two people having the same birthday.
+And even with 23 people, there is a chance of 50% of that happens.
+Write a program to experiment this problem.
+"""
