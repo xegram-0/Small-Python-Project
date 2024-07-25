0179174444bd462ec3b2bafcc2d937bff342a2b4
HONG NHAN <nguyenhongnhan9a5@gmail.com>
2024-07-25 17:35:58 +0700

    Added codes and comments to collatz_sequence.py

--- collatz_sequence.py
+++ collatz_sequence.py
@@ -3,15 +3,18 @@ import sys
 
 def collatzSequence(number):
     numberList: list = [number]
-    while True:
+    nextNumber: int = 0
+    while number != 1:
         if number % 2 == 0:
-            numberList.append(number/2)
+            nextNumber = number // 2
+            numberList.append(nextNumber)
+            # print(numberList)
         elif number % 2 == 1:
-            numberList.append(number*3 + 1)
-        elif number == 1:
-            return numberList
-        else:
-            continue
+            nextNumber = number * 3 + 1
+            numberList.append(nextNumber)
+            # print(numberList)
+        number = nextNumber
+    return numberList
 
 
 def main():
@@ -19,10 +22,10 @@ def main():
           1. If n is even, next number is n/2.
           2. If n is odd, next number is n*3 + 1.
           3. If n = 1, stop. Else repeat.""")
-
-    print("Enter a number greater than 0 or QUIT")
-    inputNumber: str = input("> ")
+    startingNumber: int = 0
     while True:
+        print("Enter a number greater than 0 or QUIT")
+        inputNumber: str = input("> ")
         if inputNumber.upper() == "QUIT":
             sys.exit()
         elif inputNumber.isdecimal() and int(inputNumber) > 0:
@@ -30,10 +33,18 @@ def main():
             break
         else:
             print("Invalid input")
-
+            continue
+    # print(startingNumber)
     collatzList: list = collatzSequence(startingNumber)
     print(collatzList)
+    print(f"There are {len(collatzList)} numbers in this sequence.")
 
 
 if __name__ == "__main__":
     main()
+"""
+1. 6 numbers.
+2. 27 numbers.
+3. Yes, since n//2 would result in even numbers that are the power of 2.
+4. My program does not allow that but with the book program, it seems to run endlessly.
+"""
