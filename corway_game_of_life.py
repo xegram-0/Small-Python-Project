4f1bda7d10dc13c5b04115f8afb8f6f382f57505
HONG NHAN NGUYEN <nguyenhongnhan9a5@gmail.com>
2024-08-14 01:17:39 +0700

    Remove placeholders

--- corway_game_of_life.py
+++ corway_game_of_life.py
@@ -1,13 +1,6 @@
-hhfmfmgffefchh# this is a placeholder
-# placeholder 2
-# place holder 3
-# placeholder 4
-# placeholder 5
-# p 6ff
-# p 7 8 9 oo10
+
 def main():
     ...
-
 
 if __name__ == "__main__":
     main()
